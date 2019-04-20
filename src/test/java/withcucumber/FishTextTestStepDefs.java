0148512ce8a115d55a8cc577591eb219a32ee72e
Lana Timofeeva <tisv2000@google.com>
2019-04-20 10:14:03 +0700

    Add cucumber java 8 implementation

--- src/test/java/withcucumber/FishTextTestStepDefs.java
+++ src/test/java/withcucumber/FishTextTestStepDefs.java
@@ -1,9 +1,6 @@
 package withcucumber;
 
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java8.En;
 import io.restassured.RestAssured;
 import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
@@ -12,65 +9,67 @@ import org.testng.Assert;
 import static common.FishTextTestUtils.*;
 import static org.hamcrest.Matchers.is;
 
-public class FishTextTestStepDefs {
+public class FishTextTestStepDefs implements En {
 
     private String type;
     private int number;
     private Response response;
 
-    @Given("^two parameters (.*) and (-?\\d+)$")
-    public void twoParametersTypeAndNumber(String type, int number) {
-        this.type = type;
-        this.number = number;
-    }
+    public FishTextTestStepDefs() {
+        Given("^two parameters (.*) and (-?\\d+)$",
+                (String type, Integer number) -> {
+                    this.type = type;
+                    this.number = number;
+                });
 
-    @When("^send GET request, specifying two parameters, in order to get json$")
-    public void sendGETRequestSpecifyingTwoParametersTypeOfTheReturningTextAndNumberOfItemsInOrderToGetJson() {
-        RestAssured.registerParser("text/html", Parser.JSON);
-        response = sendRequest(type, number, FORMAT_JSON);
-    }
+        When("^send GET request, specifying two parameters, in order to get json$",
+                () -> {
+                    RestAssured.registerParser("text/html", Parser.JSON);
+                    response = sendRequest(type, number, FORMAT_JSON);
+                });
 
-    @Then("^the status code should be (\\d+)$")
-    public void theStatusCodeShouldBe(int statusCode) {
-        response.then().statusCode(statusCode);
-    }
+        Then("^the status code should be (\\d+)$",
+                (Integer statusCode) -> {
+                    response.then().statusCode(statusCode);
+                });
 
-    @And("^result field in json should be '(.*)'$")
-    public void resultFieldInJsonShouldBeSuccess(String expectedStatus) {
-        response.then().body(JSON_FIELD_STATUS, is(expectedStatus));
-    }
+        And("^result field in json should be '(.*)'$",
+                (String expectedStatus) -> {
+                    response.then().body(JSON_FIELD_STATUS, is(expectedStatus));
+                });
 
-    @And("^number of elements in json should equal (\\d+)$")
-    public void numberOfElementsShouldEqualExpected(int expectedNumber) {
-        Assert.assertEquals(countItemsNumber(response.jsonPath().getString(JSON_FIELD_TEXT), type, FORMAT_JSON),
-                expectedNumber, getInvalidAmountMessage(type));
-    }
+        And("^number of elements in json should equal (\\d+)$",
+                (Integer expectedNumber) -> {
+                    Assert.assertEquals((Integer) countItemsNumber(response.jsonPath().getString(JSON_FIELD_TEXT), type, FORMAT_JSON),
+                            expectedNumber, getInvalidAmountMessage(type));
+                });
 
 
-    @When("^send GET request, specifying two parameters, in order to get html$")
-    public void sendGETRequestSpecifyingTwoParametersTypeOfTheReturningTextAndNumberOfItemsInOrderToGetHtml() {
-        response = sendRequest(type, number, FORMAT_HTML);
-    }
+        When("^send GET request, specifying two parameters, in order to get html$",
+                () -> {
+                    response = sendRequest(type, number, FORMAT_HTML);
+                });
 
-    @And("^number of elements in html should equal (\\d+)$")
-    public void numberOfElementsInHtmlShouldEqualExpected(int expectedNumber) {
-        Assert.assertEquals(countItemsNumber(response.then().extract().asString(), type, FORMAT_HTML),
-                expectedNumber, getInvalidAmountMessage(type));
-    }
+        And("^number of elements in html should equal (\\d+)$",
+                (Integer expectedNumber) -> {
+                    Assert.assertEquals((Integer) countItemsNumber(response.then().extract().asString(), type, FORMAT_HTML),
+                            expectedNumber, getInvalidAmountMessage(type));
+                });
 
-    @And("^json error code should equal (\\d+)$")
-    public void jsonErrorCodeShouldEqualExpected(int errorCode) {
-        response.then().body(JSON_FIELD_ERROR_CODE, is(errorCode));
-    }
+        And("^json error code should equal (\\d+)$",
+                (Integer errorCode) -> {
+                    response.then().body(JSON_FIELD_ERROR_CODE, is(errorCode));
+                });
 
-    @And("^json error text should equal (.*)$")
-    public void jsonErrorTextShouldEqualErrorText(String errorText) {
-        response.then().body(JSON_FIELD_TEXT, is(errorText));
-    }
+        And("^json error text should equal (.*)$",
+                (String errorText) -> {
+                    response.then().body(JSON_FIELD_TEXT, is(errorText));
+                });
 
-    @And("^html text should equal (.*)$")
-    public void htmlTextShouldEqualErrorText(String errorText) {
-        Assert.assertEquals(response.then().extract().asString(), errorText);
+        And("^html text should equal (.*)$",
+                (String errorText) -> {
+                    Assert.assertEquals(response.then().extract().asString(), errorText);
+                });
     }
 
 }
